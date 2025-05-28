98c517f2800593696cdcb8b7318c4c8fc91ebaf5
Saikaranam-70 <saikaranam46@gmail.com>
2025-05-28 18:33:07 +0530

    first commit

--- src/main/java/com/FuelBee/backend/controller/DealerController.java
+++ src/main/java/com/FuelBee/backend/controller/DealerController.java
@@ -1,9 +1,18 @@
 package com.FuelBee.backend.controller;
 
+import com.FuelBee.backend.service.impl.DealerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/dealer")
 public class DealerController {
+    @Autowired
+    private DealerService dealerService;
+
+    @PostMapping("/phone-register")
+    public ResponseEntity<>
 }
--- src/main/java/com/FuelBee/backend/response/DealerResponse.java
+++ src/main/java/com/FuelBee/backend/response/DealerResponse.java
@@ -1,5 +1,37 @@
 package com.FuelBee.backend.response;
 
 public class DealerResponse {
+    private String message;
+    private String id;
+    private String statusCode;
 
+    public DealerResponse(String message, String id, String statusCode) {
+        this.message = message;
+        this.id = id;
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
 }
